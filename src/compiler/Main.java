347e11ebb080b766abeee8ed74cb7d850cb69735
CMM2212 <colinnmclean@gmail.com>
2025-05-28 10:51:38 -0500

    Clean up main and fix gitignore

--- src/compiler/Main.java
+++ src/compiler/Main.java
@@ -1,35 +1,44 @@
 package compiler;
 
+import compiler.argparse.ArgumentParser;
 import compiler.errors.*;
 import compiler.lexer.Lexer;
 import compiler.parser.Parser;
-import compiler.prettyprinter.PrettyPrinter;
 import compiler.tac.IntermediateCodeGenerator;
 import compiler.tac.IntermediateCodePrinter;
 import compiler.typechecker.TypeChecker;
 
-import java.io.IOException;
-
+/**
+ * Entry point for the compiler.
+ *
+ *
+ */
 public class Main {
     public static void main(String[] args) {
+        ArgumentParser argumentParser = ArgumentParser.parseArguments(args);
+        Lexer lexer = new Lexer(argumentParser.inputFilename());
 
-        String filename = "input.txt";
-        Lexer lexer = new Lexer(filename);
-        ErrorPrinter errorPrinter = new ErrorPrinter();
         try {
-            Parser parser = new Parser(lexer);
-            TypeChecker typeChecker = new TypeChecker(parser);
-            //PrettyPrinter printer = new PrettyPrinter(parser);
-            IntermediateCodeGenerator interCode = new IntermediateCodeGenerator(typeChecker);
-            IntermediateCodePrinter interCodePrinter = new IntermediateCodePrinter(interCode);
-            System.out.println("Starting lexical analysis...\n");
+            compile(lexer, argumentParser.outputFilename());
+        } catch (CompilerException e) {
+            printError(e, lexer);
+            System.exit(1);
         }
-        catch (CompilerException e) {
-                if (e instanceof LexicalException || e instanceof SyntaxException) {
-                    errorPrinter.printError(lexer.getErrorContext(), e);
-                } else if (e instanceof TypeException) {
-                    errorPrinter.printError(lexer.getErrorContext().lines, lexer.getErrorContext(), (TypeException) e);
-                }
-        }
+    }
+
+    private static void compile(Lexer lexer, String outputFilename) {
+        Parser parser = new Parser(lexer);
+        TypeChecker typeChecker = new TypeChecker(parser);
+        IntermediateCodeGenerator interCode = new IntermediateCodeGenerator(typeChecker);
+        IntermediateCodePrinter interCodePrinter = new IntermediateCodePrinter(interCode, outputFilename);
+    }
+
+    private static void printError(CompilerException e, Lexer lexer){
+        ErrorPrinter errorPrinter = new ErrorPrinter();
+            if (e instanceof LexicalException || e instanceof SyntaxException) {
+                errorPrinter.printError(lexer.getErrorContext(), e);
+            } else if (e instanceof TypeException) {
+                errorPrinter.printError(lexer.getErrorContext().lines, lexer.getErrorContext(), (TypeException) e);
+            }
     }
 }
---
+++ src/compiler/argparse/ArgumentParser.java
@@ -0,0 +1,37 @@
+﻿package compiler.argparse;
+
+public record ArgumentParser(String inputFilename, String outputFilename) {
+
+    public static ArgumentParser parseArguments(String[] args) {
+        // Default values
+        String inputFile = "input.txt";
+        String outputFile = null;
+
+        for (int i = 0; i < args.length; i++)
+            switch (args[i]) {
+                case "-i":
+                case "--input":
+                    inputFile = args[++i];
+                case "-o":
+                case "--output":
+                    outputFile = args[++i];
+                case "-h":
+                case "--help":
+                    printHelp();
+                    System.exit(0);
+                default:
+                    System.err.println("Unknown argument: " + args[i]);
+                    printHelp();
+                    System.exit(1);
+            }
+        return new ArgumentParser(inputFile, outputFile);
+    }
+
+    private static void printHelp() {
+        System.out.println("Usage: my-compiler [options]");
+        System.out.println("Options:");
+        System.out.println("  -i, --input <file>    Specify the input file to compile.");
+        System.out.println("  -o, --output <file>   Specify the output file to write the compiled code to.");
+        System.out.println("  -h, --help            Print this help message.");
+    }
+}
